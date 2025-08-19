bacaf4bbfae862abc6e0d69b1efe044d173725f7
sreecharan1289 <charan.ambatipally5@gmail.com>
2025-08-19 23:57:25 +0530

    created backend file and added app.py and requirements

--- backend/app.py
+++ backend/app.py
@@ -3,13 +3,25 @@ from flask_cors import CORS
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
+
+# import os
+
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port)
+
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000"])  # Allow Next.js origin
+CORS(app, origins=["http://localhost:3000, https://chat-bot-intent-classifier-efrs-j1quwzj2p.vercel.app/"])  # Allow Next.js origin
 
 # Load your ATIS model
-MODEL_PATH = r"C:\Users\Sree Charan\Downloads\intent_model"
-tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
-model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH)
+# MODEL_PATH = r"C:\Users\Sree Charan\Downloads\intent_model"
+# tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
+# model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH)
+
+MODEL_NAME = "Sreecharan1289/intent-model"  # Your HF model name
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
+
 
 # Use the model's id2label mapping
 id2label = model.config.id2label
@@ -56,3 +68,8 @@ def health():
 if __name__ == '__main__':
     print("Starting ATIS Intent Classification API...")
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    print("Starting ATIS Intent Classification API...")
+    app.run(host="0.0.0.0", port=port)
